d7e1c1f6c6ae797db626ff0708a0e1adb0eb4520
Delegus <189108385+Delegus@users.noreply.github.com>
2025-11-14 16:57:07 -0600

    ItemTypes
    Check and change certain item types so it doesn't have to be done on the site.

--- D2TxtImporter.lib/Model/Dictionaries/ItemTypes.cs
+++ D2TxtImporter.lib/Model/Dictionaries/ItemTypes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace D2TxtImporter.lib.Model.Dictionaries
@@ -42,9 +43,30 @@ namespace D2TxtImporter.lib.Model.Dictionaries
 
             foreach (var row in table)
             {
+                // Normalize specific ItemType names before using them
+                var rawIndex = row["ItemType"];
+                var normalizedIndex = rawIndex;
+                if (!string.IsNullOrWhiteSpace(rawIndex))
+                {
+                    var idx = rawIndex.Trim();
+                    if (idx.Equals("Merc Equip", StringComparison.OrdinalIgnoreCase))
+                    {
+                        idx = "Helm";
+                    }
+                    else if (idx.Equals("medium charm", StringComparison.OrdinalIgnoreCase))
+                    {
+                        idx = "Large Charm";
+                    }
+                    else if (idx.Equals("large charm", StringComparison.OrdinalIgnoreCase))
+                    {
+                        idx = "Grand Charm";
+                    }
+                    normalizedIndex = idx;
+                }
+
                 var itemType = new ItemType
                 {
-                    Index = row["ItemType"],
+                    Index = normalizedIndex,
                     Code = row["Code"],
                     Equiv1 = row["Equiv1"],
                     Equiv2 = row["Equiv2"],
--- D2TxtImporter.lib/Model/Types/ItemProperty.cs
+++ D2TxtImporter.lib/Model/Types/ItemProperty.cs
@@ -40,7 +40,8 @@ namespace D2TxtImporter.lib.Model.Types
         public string Suffix { get; set; }
 
         [JsonIgnore]
-        private static List<string> _ignoredProperties = new List<string> { "state", "bloody", "oskill_hide", "dmg-throw", "uberstone-property" };
+        private static List<string> _ignoredProperties = new List<string> { "state", "bloody", 
+            "oskill_hide", "dmg-throw", "uberstone-property", "ubertorch-property" };
 
         [JsonIgnore]
         public string CompareKey => ItemStatCost.Stat + Parameter;
